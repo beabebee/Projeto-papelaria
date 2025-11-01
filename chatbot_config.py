7539ec15da1aaff4a7e1776a7d99487314da1bb7
beabebee <b.digrocco@aluno.ifsp.edu.br>
2025-11-01 12:55:01 -0300

    faq

--- chatbot_config.py
+++ chatbot_config.py
@@ -4,8 +4,11 @@ from sklearn.metrics.pairwise import cosine_similarity
 # Nosso "cérebro" do assistente continua o mesmo
 conversa_assistente = {
     "como faço para cadastrar um novo produto?": "Na página inicial, clique em 'Produtos'. Na tela de listagem, procure e clique no botão para 'Cadastrar Produto'.",
+    "onde posso acessar a lista de produtos disponíveis?": "Acesse pelo botão 'Produtos' na páginas inicial.",
     "onde eu vejo a lista de clientes?": "Clique no botão 'Clientes' no menu da página inicial para ver, editar ou cadastrar novos clientes.",
+    "como eu vejo a lista de vendas registradas?": "Acesse a seção 'Vendas' na página inicial",
     "como eu registro uma nova venda no sistema?": "Acesse a seção 'Vendas' a partir do menu inicial e clique em 'Nova Venda'. Você precisará selecionar o cliente e os produtos vendidos.",
+    "é possível saber quais produtos recomendar a um cliente?": "Na página inicial, clique em 'Clientes'. Ao lado do botão 'Editar' você deve encontrar o botão que recomenda ao cleinte com base em suas ultimas compras.",
     "é possível alterar os dados de um cliente?": "Na lista de clientes, encontre o cliente desejado e clique na opção 'Editar' para atualizar as informações.",
     "e se eu errar o preço de um produto, posso corrigir?": "Para corrigir os dados de um produto, vá até a lista de 'Produtos', encontre o item e clique em 'Editar' para corrigir o preço e outras informações.",
     "o que devo fazer se eu esquecer minha senha?": "Na tela de login, clique no link 'Esqueci minha senha'. O sistema te guiará para criar uma nova senha através do seu e--mail.",
